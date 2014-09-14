0d2e79878ef1eac0a823777c7126f3c38d76a191
Benjy Roberts <benjyroberts@gmail.com>
2014-09-14 04:06:58 +0000

    get pic data works.

--- getPics.php
+++ getPics.php
@@ -1,43 +1,35 @@
 <?php
 require 'database.php';
 
-$pageIndex = 1;//htmlentities($_POST['pageIndex']);
+$pageIndex = (int)htmlentities($_POST['pageIndex']);
 $startingPicIndex = 5 * $pageIndex;
 
 $stmt = $mysqli->prepare(
         sprintf("select name, rating, fileName, timeCreated from pics order by timeCreated LIMIT %s,5", $startingPicIndex));
 if(!$stmt){
     printf("Query Prep Failed: %s\n", $mysqli->error);
+    echo json_encode(array(
+        'success' => False,
+        'message' => 'DB query fail'
+        ));
     exit;
 }
-
 $stmt->execute();
-
 $stmt->bind_result($name, $rating, $fileName, $timeCreated);
- 
 $data = array();
- 
 while($stmt->fetch()) {
     $element = array(
         'name' => $name,
         'rating' => $rating,
-        '$fileName' => $fileName,
+        'fileName' => $fileName,
         'timestamp' => $timeCreated
-    );
+        );
     array_push($data, $element);
 }
-echo json_encode($data);
-
 $stmt->close();
-
-//$results = array();
-//while($row = mysql_fetch_array($sql))
-//{
-//   $results[] = array(
-//      'title' => base64_decode($row['title']),
-//      'price' => $row['price'],
-//      'seller_user' => $row['user']
-//   );
-//}
-//$json = json_encode($results);
+echo json_encode(array(
+    'success' => True,
+    'data' => $data
+    ));
+exit;
 ?>
--- js/main.js
+++ js/main.js
@@ -72,7 +72,7 @@ function createEnlargedVersion(img){
 function loadPic(num){
 	num = num || 5;
 	return createRandomImages(num);
-	//return getPics(num);
+	//getPicsData(0);
 }
 
 function setUpPictureLoading(){
@@ -93,30 +93,22 @@ function setUpPictureLoading(){
 		});
 }
 
-function getPics(numberOfPics){
-
-	//call picture finder
-
-
-
-	var filenames;
-	return filenames;
-
-	//send ajax request 
-	var dataPic;
- 
-	var xmlHttp = new XMLHttpRequest(); // Initialize our XMLHttpRequest instance
-	xmlHttp.open("POST", "login_ajax.php", true); // Starting a POST request (NEVER send passwords as GET variables!!!)
-	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // It's easy to forget this line for POST requests
-	xmlHttp.addEventListener("load", function(event){
-		var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
-		if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
-			alert("You've been Logged In!");
-		}else{
-			alert("You were not logged in.  "+jsonData.message);
+function getPicsData(pageIndex) {	
+	$.ajax({
+	type: 'POST',
+	url: 'getPics.php',
+	data: {'pageIndex': pageIndex},
+	success: function(msg) {
+		var jsonData = JSON.parse(msg);
+		data = jsonData.data;
+		for (i = 0; i < data.length; i++) {
+			picData = data[i];
+			alert(picData.name);
+			//TODO
+			//add the images to the list.
 		}
-	}, false); // Bind the callback to the load event
-	xmlHttp.send(dataPic); // Send the data
+	}
+	});
 
 }
 
@@ -140,10 +132,6 @@ function openUploadDiv(){
 			$("#fileImage").click(function(){
 				console.log("hi");
 				$("#file").change(function(){
-
-
-
-
 					var files = this.files || [];
 					fileDisplayArea = document.getElementById("imagePreveiw");
 					console.log(files[0]);
