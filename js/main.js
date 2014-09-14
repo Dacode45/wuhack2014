1b7386f9afec429d65dbc5a690deefdc42df7ce3
LawranceFung <fungjl@wustl.edu>
2014-09-13 20:59:56 -0500

    More shit

--- js/main.js
+++ js/main.js
@@ -102,7 +102,21 @@ function getPics(numberOfPics){
 	var filenames;
 	return filenames;
 
-	//send ajax request
+	//send ajax request 
+	var dataPic;
+ 
+	var xmlHttp = new XMLHttpRequest(); // Initialize our XMLHttpRequest instance
+	xmlHttp.open("POST", "login_ajax.php", true); // Starting a POST request (NEVER send passwords as GET variables!!!)
+	xmlHttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded"); // It's easy to forget this line for POST requests
+	xmlHttp.addEventListener("load", function(event){
+		var jsonData = JSON.parse(event.target.responseText); // parse the JSON into a JavaScript object
+		if(jsonData.success){  // in PHP, this was the "success" key in the associative array; in JavaScript, it's the .success property of jsonData
+			alert("You've been Logged In!");
+		}else{
+			alert("You were not logged in.  "+jsonData.message);
+		}
+	}, false); // Bind the callback to the load event
+	xmlHttp.send(dataPic); // Send the data
 
 }
 
