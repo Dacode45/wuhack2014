ecf24ba29a061f8711513f4e990a6f3336d1bef3
Nathan Gitter <nathan@nathans-mbp.dhcp.wustl.edu>
2014-09-13 19:51:56 -0500

    updating drawer style

--- js/main.js
+++ js/main.js
@@ -98,7 +98,7 @@ function openUploadDiv(){
 			var uploadDiv = createUploadDiv();
 			$("#footer").append(uploadDiv);
 			
-			$("#footer").animate({height: "40%"}).append(uploadDiv);
+			$("#footer").animate({height: "100px"}).append(uploadDiv);
 			$("#fileImage").click(function(){
 				console.log("hi");
 				$("#file").change(function(){
@@ -143,7 +143,7 @@ function openUploadDiv(){
 
 function closeUploadDiv(up){
 
-	$("#footer").animate({height: "10%"})
+	$("#footer").animate({height: "20px"})
 	$(uploadDiv).remove();
 	
 	$("#upload-button").off("click", closeUploadDiv);
